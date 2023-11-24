4e605cce0a646d77f32d77949a3fe86333e3d1a4
seymur4ik <seymurhszd@gmail.com>
2023-11-24 12:25:36 -0800

    test

---
+++ variadic_functions/main_0.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+  print_all("fcsi", 3.14435, 'H' '#Cisfun', 0);
+  return (0);
+}
