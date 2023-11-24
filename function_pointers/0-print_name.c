fe72771cc1eacab2a3ea41f5c74deb0c19ce2184
seymur4ik <146814373+seymur4ik@users.noreply.github.com>
2023-11-25 00:02:52 +0400

    Bug Fix
    Bug Fix

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - prints name
+ * @name: name to print (char *)
+ * @f: callback function (pointer to the void func.)
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
