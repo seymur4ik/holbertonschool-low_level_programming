e5d2ba1e18a69124fd754daa163985420b7d985d
seymur4ik <seymurhszd@gmail.com>
2023-12-15 11:23:40 -0800

    test

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
-* hash_djb2 - Hash function implementing the djb2 algorithm.
-* @str: The string to hash.
-*
-* Return: The calculated hash.
-*/
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
@@ -13,7 +13,7 @@ int c;
 
 hash = 5381;
 while ((c = *str++))
-hash = ((hash << 5)  +  hash)  +  c;
+hash = ((hash << 5) + hash) + c;
 
 return (hash);
 }
