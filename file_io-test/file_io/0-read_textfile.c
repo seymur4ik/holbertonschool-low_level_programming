5cd187a4897124df0c3df0df89710d75cd35ea2d
seymur4ik <seymurhszd@gmail.com>
2023-12-15 01:19:22 -0800

    test file update

---
+++ file_io-test/file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ *	to the POSIX standard output
+ * @filename: filename
+ * @letters: letters
+ * Return: if the file can not be opened or read, return 0
+ *	if filename is NULL return 0
+ *	if write fails or does not write
+ *	the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n, w;
+	char *buf = malloc(sizeof(char) * letters + 1);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	n = read(fd, buf, letters);
+
+	w = write(STDOUT_FILENO, buf, n);
+
+	if (fd == -1 || n == -1 || w == -1 || w != n)
+		w = 0;
+
+	free(buf);
+	close(fd);
+
+	return (w);
+}
