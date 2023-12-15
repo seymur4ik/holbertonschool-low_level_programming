326cbf7ab77892f1e684e34d728e6b4871bfd197
seymur4ik <seymurhszd@gmail.com>
2023-12-15 02:30:32 -0800

    1-djb2.c file fix

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
- *
- * Return: The calculated hash.
- */
+* hash_djb2 - Hash function implementing the djb2 algorithm.
+* @str: The string to hash.
+*
+* Return: The calculated hash.
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
